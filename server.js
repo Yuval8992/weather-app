442687170782f1db7b6be9f202d5b9465f339292
yuval.shlomo <yuval8992@gmail.com>
2021-05-20 06:42:49 +0300

    create server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/weather-app"));
+app.use(express.static("./dist/weather-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/weather-app/index.html"));
+  res.sendFile("index.html", { root: "dist/weather-app/" });
 });
 
 // Start the app by listening on the default Heroku port
